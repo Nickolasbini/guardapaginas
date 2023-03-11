c53f5c783e5748d37dbfb1d43ba5d7d9a4d149c6
Nickolasbini <nickolasbini@hotmail.com>
2023-03-10 21:28:14 -0300

    bd schema

--- app/src/main/java/com/br/guardapaginas/MainActivity.java
+++ app/src/main/java/com/br/guardapaginas/MainActivity.java
@@ -35,8 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
             SQLiteDatabase db = openOrCreateDatabase("DB_GUARDAPAGINAS", MODE_PRIVATE, null);
             // Create Table
-            db.execSQL("CREATE TABLE IF NOT EXISTS gender(id INTEGER PRIMARY KEY, name varchar(300), createdAt varchar(50))");
-            db.execSQL("INSERT INTO gender(name, createdAt) VALUES ('drama', '2022-03-08')");
+            db.execSQL("CREATE TABLE IF NOT EXISTS genders(id INTEGER PRIMARY KEY, name varchar(300), createdAt varchar(50))");
+            db.execSQL("CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, title varchar(100), synopsis varchar(300), author varchar(60), releaseDate varchar(50), editorName varchar(60), gender INTEGER, FOREIGN KEY (gender) REFERENCES genders (id))");
+            db.execSQL("CREATE TABLE IF NOT EXISTS institutions(id INTEGER PRIMARY KEY, name varchar(200), address varchar(300), number varchar(50), telephone varchar(30), email varchar(30))");
+            db.execSQL("CREATE TABLE IF NOT EXISTS users(id PRIMARY KEY, name varchar(200), cpf varchar(10), email varchar(30), password varchar(65), status varchar(2))");
+            db.execSQL("CREATE TABLE IF NOT EXISTS bookBorrowings(id PRIMARY KEY, book INTEGER, institution INTEGER, lendData varchar(50), expectedDelivery varchar(50), realDelivery varchar(50), reader INTEGER, FOREIGN KEY (reader) REFERENCES users (id), FOREIGN KEY (book) REFERENCES books (id),FOREIGN KEY (institution) REFERENCES institutions (id))");
+            //db.execSQL("INSERT INTO gender(name, createdAt) VALUES ('drama', '2022-03-08')");
 
             String query   = "SELECT * FROM gender";
             Cursor results = db.rawQuery(query, null);
